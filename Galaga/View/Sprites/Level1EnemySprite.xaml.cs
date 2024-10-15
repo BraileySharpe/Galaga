4e75b2b40fd9a89be1063bb59d1d138e08283d0d
Brailey Sharpe <bs00201@my.westga.edu>
2024-10-15 18:15:55 -0400

    Updated player sprite. Fixed Level 1 Enemy XAML.
    Updated the appearance of the player sprite, resulting in a sizing change. Also fixed level 1 enemy, because it was accidentally created as a XAML view instead of a user control.

---
+++ Galaga/View/Sprites/Level1EnemySprite.xaml.cs
@@ -0,0 +1,21 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Galaga.View.Sprites
+{
+    /// <summary>
+    /// The level 1 enemy sprite.
+    /// </summary>
+    /// <seealso cref="Galaga.View.Sprites.BaseSprite" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector2" />
+    public sealed partial class Level1EnemySprite
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Level1EnemySprite"/> class.
+        /// </summary>
+        public Level1EnemySprite()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
