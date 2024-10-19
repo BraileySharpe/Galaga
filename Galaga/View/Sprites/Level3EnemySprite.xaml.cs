df68e0de711be50650733ac51195b7bc3783bb0e
Brailey Sharpe <bs00201@my.westga.edu>
2024-10-19 14:17:57 -0400

    Added Level 3 Enemy Sprite.

---
+++ Galaga/View/Sprites/Level3EnemySprite.xaml.cs
@@ -0,0 +1,12 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Galaga.View.Sprites
+{
+    public sealed partial class Level3EnemySprite
+    {
+        public Level3EnemySprite()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
