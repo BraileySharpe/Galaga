31b3992bd868333cf3cd0539ef9fb3377c8794b7
Brailey Sharpe <bs00201@my.westga.edu>
2024-11-10 01:29:46 -0500

    Added Shooting Enemy class derived from Enemy.

---
+++ Galaga/Model/ShootingEnemy.cs
@@ -0,0 +1,52 @@
+﻿using Galaga.View.Sprites;
+
+namespace Galaga.Model
+{
+    /// <summary>
+    ///     Represents an enemy capable of shooting in the game.
+    /// </summary>
+    public class ShootingEnemy : Enemy
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets a value indicating whether this enemy is capable of shooting.
+        /// </summary>
+        public bool CanShoot { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ShootingEnemy" /> class.
+        /// </summary>
+        /// <param name="sprite">The enemy sprite.</param>
+        public ShootingEnemy(BaseSprite sprite) : base(sprite)
+        {
+            this.CanShoot = true;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     Enables this enemy to shoot.
+        /// </summary>
+        public void EnableShooting()
+        {
+            this.CanShoot = true;
+        }
+
+        /// <summary>
+        ///     Disables this enemy from shooting.
+        /// </summary>
+        public void DisableShooting()
+        {
+            this.CanShoot = false;
+        }
+
+        #endregion
+    }
+}
